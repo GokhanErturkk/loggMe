118a92d260d948a5ad6b5db40915c8461ad0bb1e
GokhanErturkk <gkn_ceza1@hotmail.com>
2024-09-02 20:34:25 +0300

    progress

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const loggMe = require('./lib/loggMe');
 
 console.log(loggMe)
 
-loggMe.setArg1("arbitraryArg")
-
-console.log(loggMe.getArg1())
+loggMe.setLogLevel("DEBUG")
+console.log(loggMe.getLogLevel())
