696a8f4cf0b0a1d15ac1b21cbdf668b209f73431
GokhanErturkk <gkn_ceza1@hotmail.com>
2024-09-03 12:46:01 +0300

    progress

---
+++ serialize_error.js
@@ -0,0 +1,55 @@
+
+
+var error = new Error('User Not Found');
+error.statusCode = 404;
+
+// console.log(serializeError(error).toObject());
+
+
+const getErrorMessage = (error) => {
+    if (error instanceof Error) {
+        return error.message
+    }
+    return ''
+}
+
+const getErrorConstructor = (error) => {
+    let constructor
+    if (error && error.constructor) {
+        constructor = error.constructor.name || error.constructor.prototype.name;
+    }
+    return constructor
+}
+
+const getErrorStack = (error) => {
+    if (error instanceof Error && error.stack != null) {
+        var stack = error.stack;
+        return stack.substring(stack.indexOf('\n') + 1); // start from the index of (the first \n +1)
+    }
+    return '';
+}
+
+const getEnumerableProperties = (error) => {
+    return Object.keys(error || {}).reduce((acc, key) => {
+            return { ...acc, [key]: error[key] }
+    }, {})
+}
+
+
+const serializeError = (error, includeStack) => {
+    var message = getErrorMessage(error);
+    var constructor = getErrorConstructor(error);
+    var stack = getErrorStack(error);
+    var enumerables = getEnumerableProperties(error)
+
+    var errorDetailsObject = {
+        ...enumerables,
+        message,
+        constructor,
+        stack
+    }
+
+    return errorDetailsObject
+}
+
+console.log(serializeError(error))
