83722e9175932961f94bfc6a94765847a7575ab7
GokhanErturkk <gkn_ceza1@hotmail.com>
2024-09-04 13:22:53 +0300

    progress

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ loggMe.setContext(
     { hostname, pid: process.pid }
 )
 
+loggMe.setContext()
 
 // console.log(loggMe.getLogLevel())
 
 
 // loggMe.info(new Error("Something unexpected happened."))  // first
 // loggMe.info(new Error("Something unexpected happened."))  // first
-// loggMe.error(new Error("Something unexpected happened."))  // first
+loggMe.error(new Error("Something unexpected happened."))  // first
 //  loggMe.warn('Warning message.')     // second
-loggMe.warn({key1:"val1"}, "warning message") // third
+// loggMe.warn({key1:"val1"}, "warning message") // third
 
 
 
